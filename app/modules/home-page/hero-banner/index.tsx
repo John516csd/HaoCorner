0a8dbbc331deb8828a60f466827c70a2b4cb09a6
John516csd <2865366928@qq.com>
2025-08-04 23:27:05 +0800

    修改样式bug

--- app/modules/home-page/hero-banner/index.tsx
+++ app/modules/home-page/hero-banner/index.tsx
@@ -36,27 +36,25 @@ const HeroBanner = () => {
     }, [])
 
     return <section className="relative h-screen w-full flex flex-col items-center justify-center bg-[#00b4ff] overflow-hidden">
-        <div className="relative flex flex-col md:flex-row items-center md:items-end justify-center md:justify-end gap-[24px] z-10 px-[24px] md:px-[40px]">
-            <div className="relative flex flex-col items-center justify-center max-w-full sm:max-w-[400px] overflow-hidden gap-4 bg-white" style={{
+        <div className="w-full relative flex flex-col md:flex-row items-center md:items-end justify-center gap-[24px] z-10 px-[24px] md:px-[40px]">
+            <div className="w-full relative flex flex-col items-center justify-center max-w-[360px] sm:max-w-[400px] overflow-hidden gap-4 bg-white" style={{
                 padding: '16px 16px 24px',
                 borderRadius: '4px',
                 boxShadow: '0 0 40px 10px rgba(0, 0, 0, 0.1)',
             }}>
-                <picture className="w-full h-full">
-                    <source srcSet="/images/me.jpg" type="image/jpg" />
-                    <img src="/images/me.jpg" alt="Hero Banner" className="w-full h-full object-cover" />
-                </picture>
+                <div className="w-full" style={{
+                    aspectRatio: '681 / 909'
+                }}>
+                    <picture className="w-full h-full">
+                        <source srcSet="/images/me.jpg" type="image/jpg" />
+                        <img src="/images/me.jpg" alt="Hero Banner" className="w-full h-full object-cover" />
+                    </picture>
+                </div>
                 <div className="w-full text-left text-lg text-black" style={{
                     fontFamily: 'Permanent Marker',
                 }}>
                     Kangding, Sichuan, China
                 </div>
-                {/* <div className="w-full flex flex-row items-center justify-start">
-                    <picture className="w-[24px] h-[24px] md:w-[32px] md:h-[32px]">
-                        <source srcSet="/images/camera.webp" type="image/webp" />
-                        <img src="/images/camera.png" alt="Hero Banner" className="w-full h-full object-cover" />
-                    </picture>
-                </div> */}
             </div>
             <div className="flex flex-col items-start justify-start gap-[24px]">
                 <picture className="w-[60px] h-[60px] md:w-[100px] md:h-[100px] ml-[24px] lg:ml-[40px]">
@@ -66,12 +64,6 @@ const HeroBanner = () => {
                 <SvgText fontSize={svgFontSize} strokeWidth={6} text="Hi, I'm Chenhao" />
             </div>
         </div>
-        {/* <div className={styles.snow_mountain}>
-            <picture className="w-full h-full">
-                <source srcSet="/images/snow-mountain.png" type="image/png" />
-                <img src="/images/snow-mountain.png" alt="Hero Banner" className="w-full h-full object-cover" />
-            </picture>
-        </div> */}
         {/* <MapLeaflet style={{
             position: 'absolute',
             bottom: '0',
