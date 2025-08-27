b1fdb5747426d1b74a1c78857e4a0784b2cfeb4f
John516csd <2865366928@qq.com>
2025-08-28 00:03:23 +0800

    添加拍立得效果

--- app/modules/home-page/hero-banner/index.tsx
+++ app/modules/home-page/hero-banner/index.tsx
@@ -1,70 +1,167 @@
-'use client'
+"use client";
 
 import SvgText from "app/components/svg-text";
-import dynamic from 'next/dynamic';
+import dynamic from "next/dynamic";
 import { useEffect, useState } from "react";
-import styles from './index.module.css';
+import styles from "./index.module.css";
 
-const MapLeaflet = dynamic(() => import('app/components/map-leaflet'), {
-    ssr: false,
+const MapLeaflet = dynamic(() => import("app/components/map-leaflet"), {
+  ssr: false,
 });
 
 const HeroBanner = () => {
-    const [svgFontSize, setSvgFontSize] = useState(80);
+  const [svgFontSize, setSvgFontSize] = useState(80);
 
-    useEffect(() => {
-        const updateFontSize = () => {
-            const width = window.innerWidth
-            if (width < 640) { // sm
-                setSvgFontSize(40)
-            } else if (width < 768) { // md
-                setSvgFontSize(50)
-            } else if (width < 1024) { // lg
-                setSvgFontSize(50)
-            } else if (width < 1280) { // xl
-                setSvgFontSize(70)
-            } else { // 2xl+
-                setSvgFontSize(80)
-            }
-        }
+  useEffect(() => {
+    const updateFontSize = () => {
+      const width = window.innerWidth;
+      if (width < 640) {
+        // sm
+        setSvgFontSize(40);
+      } else if (width < 768) {
+        // md
+        setSvgFontSize(50);
+      } else if (width < 1024) {
+        // lg
+        setSvgFontSize(50);
+      } else if (width < 1280) {
+        // xl
+        setSvgFontSize(70);
+      } else {
+        // 2xl+
+        setSvgFontSize(80);
+      }
+    };
 
-        updateFontSize()
+    updateFontSize();
 
-        window.addEventListener('resize', updateFontSize)
+    window.addEventListener("resize", updateFontSize);
 
-        return () => window.removeEventListener('resize', updateFontSize)
-    }, [])
+    return () => window.removeEventListener("resize", updateFontSize);
+  }, []);
 
-    return <section className="relative h-screen w-full flex flex-col items-center justify-center bg-[#00b4ff] overflow-hidden">
-        <div className="w-full relative flex flex-col md:flex-row items-center md:items-end justify-center gap-[24px] z-10 px-[24px] md:px-[40px]">
-            <div className="w-full relative flex flex-col items-center justify-center max-w-[360px] sm:max-w-[400px] overflow-hidden gap-4 bg-white" style={{
-                padding: '16px 16px 24px',
-                borderRadius: '4px',
-                boxShadow: '0 0 40px 10px rgba(0, 0, 0, 0.1)',
-            }}>
-                <div className="w-full" style={{
-                    aspectRatio: '681 / 909'
-                }}>
-                    <picture className="w-full h-full">
-                        <source srcSet="/images/me.jpg" type="image/jpg" />
-                        <img src="/images/me.jpg" alt="Hero Banner" className="w-full h-full object-cover" />
-                    </picture>
-                </div>
-                <div className="w-full text-left text-lg text-black" style={{
-                    fontFamily: 'Permanent Marker',
-                }}>
-                    Kangding, Sichuan, China
-                </div>
-            </div>
-            <div className="flex flex-col items-start justify-start gap-[24px]">
-                <picture className="w-[60px] h-[60px] md:w-[100px] md:h-[100px] ml-[24px] lg:ml-[40px]">
-                    <source srcSet="/images/halo.webp" type="image/webp" />
-                    <img src="/images/halo.png" alt="Hero Banner" className="w-full h-full object-cover" />
+  return (
+    <section className={styles.hero_banner}>
+      <div className={styles.main_container}>
+        <div className={styles.card_container}>
+          <div className={styles.card_inner}>
+            {/* 正面 */}
+            <div className={styles.front_layer}>
+              <div className={styles.image_container}>
+                <picture className={styles.picture_full}>
+                  <source srcSet="/images/me.jpg" type="image/jpg" />
+                  <img
+                    src="/images/me.jpg"
+                    alt="Hero Banner"
+                    className={styles.image_full}
+                  />
                 </picture>
-                <SvgText fontSize={svgFontSize} strokeWidth={6} text="Hi, I'm Chenhao" />
+              </div>
+              <div className={styles.location_text}>
+                Kangding, Sichuan, China
+              </div>
             </div>
+            {/* 背面 */}
+            <div className={styles.back_layer}>
+              {/* Don't put in mouth */}
+              <div className={styles.top_bar}>
+                <div className={styles.top_bar_text_wrapper}>
+                  <div className={styles.top_bar_text}>
+                    <div className={styles.dont_put_in_mouth_img_container}>
+                      <img
+                        src="/images/dont-put-in-mouth.png"
+                        alt="Don't put in mouth"
+                        className={styles.dont_put_in_mouth_img}
+                      />
+                    </div>{" "}
+                    Don't put in mouth
+                  </div>
+                  <div className={styles.top_bar_text}>
+                    <div className={styles.dont_put_in_mouth_img_container}>
+                      <img
+                        src="/images/dont-put-in-mouth.png"
+                        alt="Don't put in mouth"
+                        className={styles.dont_put_in_mouth_img}
+                      />
+                    </div>{" "}
+                    Don't put in mouth
+                  </div>
+                  <div className={styles.top_bar_text}>
+                    <div className={styles.dont_put_in_mouth_img_container}>
+                      <img
+                        src="/images/dont-put-in-mouth.png"
+                        alt="Don't put in mouth"
+                        className={styles.dont_put_in_mouth_img}
+                      />
+                    </div>{" "}
+                    Don't put in mouth
+                  </div>
+                  <div className={styles.top_bar_text}>
+                    <div className={styles.dont_put_in_mouth_img_container}>
+                      <img
+                        src="/images/dont-put-in-mouth.png"
+                        alt="Don't put in mouth"
+                        className={styles.dont_put_in_mouth_img}
+                      />
+                    </div>{" "}
+                    Don't put in mouth
+                  </div>
+                </div>
+              </div>
+              {/* instax FUJIFILM */}
+              <div className={styles.instax_container}>
+                <div className={styles.instax_img_wrapper}>
+                  <div className={styles.instax_img_container}>
+                    <img
+                      src="/images/instax.png"
+                      alt="instax FUJIFILM"
+                      className={styles.instax_img}
+                    />
+                  </div>
+                  <div className={styles.instax_img_container}>
+                    <img
+                      src="/images/instax.png"
+                      alt="instax FUJIFILM"
+                      className={styles.instax_img}
+                    />
+                  </div>
+                  <div className={styles.instax_img_container}>
+                    <img
+                      src="/images/instax.png"
+                      alt="instax FUJIFILM"
+                      className={styles.instax_img}
+                    />
+                  </div>
+                  <div className={styles.instax_img_container}>
+                    <img
+                      src="/images/instax.png"
+                      alt="instax FUJIFILM"
+                      className={styles.instax_img}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        {/* <MapLeaflet style={{
+        <div className={styles.right_container}>
+          <picture className={styles.halo_picture}>
+            <source srcSet="/images/halo.webp" type="image/webp" />
+            <img
+              src="/images/halo.png"
+              alt="Hero Banner"
+              className={styles.image_full}
+            />
+          </picture>
+          <SvgText
+            fontSize={svgFontSize}
+            strokeWidth={6}
+            text="Hi, I'm Chenhao"
+          />
+        </div>
+      </div>
+
+      {/* <MapLeaflet style={{
             position: 'absolute',
             bottom: '0',
             right: '0',
@@ -75,7 +172,8 @@ const HeroBanner = () => {
             border: '10px solid #fff',
             borderRadius: '100%',
         }} /> */}
-    </section>;
+    </section>
+  );
 };
 
 export default HeroBanner;
